47ea61e7fad02a195b09a8a7e87b9daa67428052
Qu1ntu <jiri.lacko123@gmail.com>
2018-07-30 21:56:51 +0200

    adding Javadocs

--- src/main/java/cz/demoapp/services/TodoService.java
+++ src/main/java/cz/demoapp/services/TodoService.java
@@ -11,8 +11,20 @@ import cz.demoapp.todo.domain.Todo;
  * Service Interface
  */
 public interface TodoService {
+	/**
+	 * Return Set of all Todos in db.
+	 */
 	public Set<Todo> getAllTodos();
+	/**
+	 * Get Todo with specific id.
+	 */
 	public Todo findById(Long Id);
+	/**
+	 * Delete Todo with specific id.
+	 */
 	public void deleteTodo(Long Id);
+	/**
+	 * Create Todo and persist it in db.
+	 */
 	public void addTodo(String desc);
 }
